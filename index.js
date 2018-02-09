1ef5891025329d7a87d9691d7e2221033103430a
johan974 <jj974@upcmail.nl>
2018-02-09 17:12:38 +0100

    Http server

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-var static = require('node-static');
-var file = new static.Server();
 
-require('http').createServer(function(request, response) {
-  request.addListener('end', function() {
-    file.serve(request, response);
-  }).resume();
-}).listen(process.env.PORT || 80);
+var static = require( 'node-static' ),
+    port = 8080,
+    http = require( 'http' );
+
+// config
+var file = new static.Server( './public', {
+    cache: 3600,
+    gzip: true
+} );
+
+// serve
+http.createServer( function ( request, response ) {
+    request.addListener( 'end', function () {
+        file.serve( request, response );
+    } ).resume();
+} ).listen( port );
