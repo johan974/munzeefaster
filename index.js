f5b9536fd22e243a949c0ac0ac59f08895c7d9b2
johan974 <jj974@upcmail.nl>
2018-02-09 19:00:17 +0100

    Update 2

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
-var sslRedirect = require('heroku-ssl-redirect');
 var express = require('express');
 var app = express();
 
-// enable ssl redirect
-app.use(sslRedirect());
-
-// app.get('/', function(req, res){
-//
-// // serve
-// http.createServer( function ( request, response ) {
-//     request.addListener( 'end', function () {
-//         file.serve( request, response );
-//     } ).resume();
-// } ).listen( port );
-
-app.use(express.static('public'));
-app.listen(8080);
+app.use(express.static( __dirname + '/public'));
+var port = process.env.PORT || 8000;
+app.listen(port);
