1f8a2a7b62f448a0faa34ade3321a7490d962d2d
johan974 <jj974@upcmail.nl>
2018-02-17 16:12:25 +0100

    Call munzee api via controller.

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -15,21 +15,3 @@ window.addEventListener('beforeinstallprompt', function(event) {
   deferredPrompt = event;
   return false;
 });
-
-function loginToMunzee() {
-  var clientid = "33a0869ef82d827b98c87235247c6f1e";
-  var redirect_uri = "https://munzeefaster.herokuapp.com/handle_oauth";
-  var munzeeRQ = "https://api.munzee.com/oauth?response_type=code&client_id=" +
-        clientid + "&redirect_uri=" + redirect_uri + "&scope=read";
-  console.log( "MunzeeURL: " + munzeeRQ);
-  fetch( munzeeRQ)
-  .then( function( response) {
-    console.log( response);
-    return response.json();
-  }).then( function( response2) {
-    console.log( response2);
-    console.log( 'Answer: ' + response2.origin);
-  }).catch( function( err) {
-    console.log( 'Error: ' + err);
-  });
-}
